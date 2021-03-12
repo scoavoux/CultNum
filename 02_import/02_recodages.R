85c17c083969b62c135a79013144e043a56f9863
COAVOUX Samuel TGI/PMD <VVXF6766@orange.com>
2021-03-12 22:12:04 +0100

    Fonctions de descriptions systématiques

---
+++ 02_import/02_recodages.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(forcats)
+
+# Netoyer les labels
+labs <- mutate(labs, 
+               varlabel = ifelse(is.na(varlabel_frombase), varlabel_fromdic, varlabel_frombase),
+               varlabel = str_wrap(varlabel, width = 40))
+
+# Collapse diplômes
+d <- mutate(d, dipl = fct_collapse(DIPLOME,
+                    `Aucun diplôme à BEPC` = c("'Jamais allé à l'école ou a quitté l'école avant la primaire'", 
+                                              "'Aucun diplôme et scolarité interrompue à la fin du primaire ou avant la fin du collège'", 
+                                              "'Aucun diplôme et scolarité jusqu''à la fin du collège ou au-delà'", 
+                                              "'CEP (certificat d''études primaires)'", 
+                                              "'BEPC, brevet élémentaire, brevet des collèges, DNB'"),
+                    CAP_BEP = "'CAP, BEP ou diplôme de niveau équivalent'", 
+                    Bac = c("'Baccalauréat général ou technologique, brevet supérieur'", 
+                            "'Capacité en droit, DAEU, ESEU'", 
+                            "'Baccalauréat professionnel, brevet professionnel, de technicien ou d''enseignement, diplôme équivalent'"), 
+                    `Bac+2` = "'BTS, DUT, Deug, Deust, diplôme de la santé ou du social de niveau bac+2, diplôme équivalent'", 
+                    `Bac+3` = "'Licence, licence pro, maîtrise, diplôme équivalent de niveau bac+3 ou bac+4'", 
+                    `Deuxieme cycle` = c("'Master, DEA, DESS, diplôme grande école niveau bac+5, doctorat de santé'", 
+                                         "'Doctorat de recherche (hors santé)'"),
+                    `NA` = c("'(NSP)'", "'(REF)'")))
+
+labs <- add_case(labs, variable = "dipl", category = "recodage", varlabel = "Diplôme recodé")
+
+# Collapse PCS
+d <- mutate(d, 
+            pcs1 = str_trunc(CSTOT, 1, ellipsis = ""),
+            pcs1 = factor(pcs1, levels = c(1:6, 9), 
+                          labels = c("Agric. expl.", "Art., comm., ce", 
+                                     "PCIS", "Prof. inter.",
+                                     "Employés", "Ouvriers",
+                                     "Inactifs")))
+
+labs <- add_case(labs, variable = "pcs1", category = "recodage", varlabel = "PCS ego niveau 1")
+
+
---
+++ 03_scripts/functions_description.R
@@ -0,0 +1,79 @@
+library(rlang)
+
+graph_qcm <- function(.vars, .value = "'Yes'", .data=d, .labs=labs){
+  select(.data, {{ .vars }}, POND_INIT) %>% 
+    pivot_longer(-POND_INIT) %>% 
+    count(name, value, wt = POND_INIT) %>% 
+    arrange(desc(n)) %>% 
+    filter(value == .value) %>% 
+    left_join(select(labs, name = "variable", lab = "varlabel")) %>% 
+    mutate(f = n / nrow(d),
+           lab = factor(lab, levels = unique(lab))) %>% 
+    ggplot(aes(lab, f, label = paste0(round(f, 2)*100, "%"))) +
+      geom_col() +
+      geom_label() +
+      coord_flip() +
+      labs(x = "Modalités", y = "Fréquence")
+}
+
+graph_qcm_indep <- function(.dep, .indep, .value = "'Yes'", .data=d, .labs=labs){
+  
+  select(.data, {{ .dep }}, {{ .indep }}, POND_INIT) %>%
+    pivot_longer({{ .dep }}) %>% 
+    count(!! enquo(.indep), name, value, wt = POND_INIT) %>%
+    group_by(!! enquo(.indep), name) %>% 
+    mutate(f = n / sum(n)) %>% 
+    ungroup() %>% 
+    arrange(desc(n)) %>%
+    filter(value == .value) %>%
+    left_join(select(labs, name = "variable", lab = "varlabel")) %>%
+    ggplot(aes(!! enquo(.indep), f, label = paste0(round(f, 2)*100, "%"))) +
+      geom_col() +
+      geom_label() +
+      coord_flip() +
+      facet_wrap(~lab) +
+      labs(x = "Modalités", y = "Fréquence", title = )
+}
+
+graph_qcm_all <- function(.dep, .value = "'Yes'", .data=d, .labs=labs){
+  graph_qcm(.vars = {{ .dep }}, .value = .value, .data = .data, .labs = .labs) %>% print()
+  
+  graph_qcm_indep(.dep = {{ .dep }}, .indep = SEXE, .value = .value, .data=d, .labs=labs) %>% print()
+  
+  graph_qcm_indep(.dep = {{ .dep }}, .indep = dipl, .value = .value, .data=d, .labs=labs) %>% print()
+
+  graph_qcm_indep(.dep = {{ .dep }}, .indep = pcs1, .value = .value, .data=d, .labs=labs) %>% print()
+}
+
+table_univar <- function(.var, .labs = labs){
+  cpt <- filter(.labs, variable == as_name(enquo(.var))) %>% pull(varlabel)
+
+  count(d, !! enquo(.var), wt = POND_INIT) %>%
+    mutate(f = round(n / sum(n) * 100),
+           f = paste0(f, "%")) %>%
+    rename(Eff. = "n", Freq. = "f") %>%
+    kable(caption = ifelse(!is.na(cpt), cpt, " "))
+}
+
+graph_univar_indep <- function(.dep, .indep, .data = d, .labs = labs) {
+  
+  cpt <- filter(.labs, variable == as_name(enquo(.dep))) %>% pull(varlabel)
+  
+  select(.data, {{ .dep }}, {{ .indep }}, POND_INIT) %>% 
+    count(!! enquo(.dep), !! enquo(.indep)) %>% 
+    group_by(!! enquo(.indep)) %>% 
+    mutate(f = n / sum(n)) %>% 
+    ggplot(aes(!! enquo(.indep), f, group = !! enquo(.dep))) +
+      geom_col(position = "dodge", aes(fill = !! enquo(.dep))) +
+      geom_label(aes(label = paste0(round(f*100), "%")), position = position_dodge(1)) +
+      coord_flip() +
+      labs(x = "Modalites", y = "Frequence", title = cpt)
+}
+
+graph_univar_all <- function(.dep, .data = d, .labs = labs){
+  table_univar({{ .dep }})
+  
+  graph_univar_indep({{ .dep }}, SEXE) %>% print()
+  graph_univar_indep({{ .dep }}, dipl) %>% print()
+  graph_univar_indep({{ .dep }}, pcs1) %>% print()
+}
